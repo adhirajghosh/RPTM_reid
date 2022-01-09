2aa14b4a1d83efacc5a94f98df3fde20fc92d5f1
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2022-01-09 21:03:47 +0100

    Create __init__.py

---
+++ datasets/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .veri import VeRi
+from .vehicleid import VehicleID
+from .duke import duke
+
+__imgreid_factory = {
+    'veri': VeRi,
+    'vehicleID': VehicleID,
+    'duke': duke,
+
+}
+
+
+def init_imgreid_dataset(name, **kwargs):
+    if name not in list(__imgreid_factory.keys()):
+        raise KeyError('Invalid dataset, got "{}", but expected to be one of {}'.format(name, list(__imgreid_factory.keys())))
+    return __imgreid_factory[name](**kwargs)
